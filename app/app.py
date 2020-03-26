e3d25b8b5ed7fce689c6c1ac6ece97e772a1e667
Tarek Ali <tarekali15@outlook.com>
2020-03-26 15:57:01 -0500

    added main server-side code; and changed frontend

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, session
+from csvquery import *
+import datetime
 
 app = Flask(__name__)
 
@@ -9,5 +11,11 @@ def main():
 @app.route('/result/', methods=["POST"])
 def result():
     input_data = {"state": request.form.get("state"), "county": request.form.get("county")}
+    data_set = get_csv(f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{date.strftime('%m-%d-%Y')}.csv")
+    query_result = data_set.already_indexed("Admin2", Comparisons.strings).query_one({
+        "Admin2": data_set["county"],
+        "Province_State": data_set["state"]
+    })
+    return render_template("result.html", query_result.to_dictionary())
       
 
