26bd22e726968a3fc5eeb53dd6c94b1f07e32667
Tarek Ali <tarekali15@outlook.com>
2020-03-26 16:48:37 -0500

    stuff changed

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, session, request
-from csvquery import *
+import csvquery
 import datetime
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ def main():
 @app.route('/result', methods=["POST"])
 def result():
     input_data = {"state": request.form.get("state"), "county": request.form.get("county")}
-    data_set = get_csv(f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{date.strftime('%m-%d-%Y')}.csv")
+    data_set = csvquery.get_csv(f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{date.strftime('%m-%d-%Y')}.csv")
     query_result = data_set.already_indexed("Admin2", Comparisons.strings).query_one({
         "Admin2": data_set["county"],
         "Province_State": data_set["state"]
