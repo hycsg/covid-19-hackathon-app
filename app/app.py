0a2bd517c4f37d92c6e1f75c4a0e01e918ae158f
Tarek Ali <tarekali15@outlook.com>
2020-03-26 16:58:01 -0500

    proper arg set

--- app/app.py
+++ app/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, session, request
-from csvquery import *
 import datetime
 
 app = Flask(__name__)
@@ -11,11 +10,11 @@ def main():
 @app.route('/result', methods=["POST"])
 def result():
     input_data = {"state": request.form.get("state"), "county": request.form.get("county")}
-    data_set = get_csv(f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{date.strftime('%m-%d-%Y')}.csv")
+    data_set = csvquery.get_csv(f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{date.strftime('%m-%d-%Y')}.csv")
     query_result = data_set.already_indexed("Admin2", Comparisons.strings).query_one({
         "Admin2": data_set["county"],
         "Province_State": data_set["state"]
     })
-    return render_template("result.html", query_result.to_dictionary())
+    return render_template("result.html", result=query_result.to_dictionary())
       
 
