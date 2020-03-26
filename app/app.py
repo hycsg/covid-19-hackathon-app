ec6c4bf7eb298bf931c2ec31e3edb86b2a4bc2d8
Tarek Ali <tarekali15@outlook.com>
2020-03-26 13:42:44 -0500

    added new route

--- app/app.py
+++ app/app.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return render_template('index.html')    
+    return render_template('index.html')
+
+@app.route('/result/', methods=["POST"])
+def result():
+    data = {"name": request.form.get("city")}
+     
+
