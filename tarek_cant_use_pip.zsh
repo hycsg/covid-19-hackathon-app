f9433e1afb2a56097195fb63ae1a32401419cf65
AlexDvorak <doe.jimmie@gmail.com>
2020-03-27 12:58:37 -0500

    case statement + cleanup

--- tarek_cant_use_pip.zsh
+++ tarek_cant_use_pip.zsh
@@ -4,23 +4,31 @@ opt=$1
 branch=$2
 cd app
 
-if [[ $opt == "run" ]]; then
-  if [[ $branch == "dev" ]]; then git checkout dev; fi;
-  if [[ $branch == "master" ]]; then git checkout master; fi
-  pipenv lock
-  pipenv install --dev
-  pipenv run flask run
-elif [[ $opt == "update" ]]; then
-  git checkout master
-  git pull
-  git checkout dev
-  git pull
-  pipenv lock
-  pipenv install --dev
-else
-  echo "Invalid command"
-  echo "------------------"
-  echo "To run the application         --> ./tooling.zsh run [branch-name]"
-  echo "To update literally everything --> ./tooling.zsh update"
-fi;
+case "$opt" in
+
+  run) # pull latest code from selected branch and run flask app
+    [ $branch == "dev" ] &&
+      git checkout dev;
+    [ $branch == "master" ] &&
+      git checkout master
+    pipenv lock &&
+    pipenv install --dev &&
+    pipenv run flask run;;
+
+  update) # grab latest code and update all pipenv deps
+    git checkout master &&
+    git pull &&
+    git checkout dev && 
+    git pull &&
+    pipenv lock &&
+    pipenv install --dev;;
+
+  *) # Invalid, print usage
+    echo "Invalid command"
+    echo "------------------"
+    echo "To run the application         --> $0 run [branch-name]"
+    echo "To update literally everything --> $0 update";;
+
+esac
+
 cd ..
